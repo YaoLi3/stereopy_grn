284b8535d80d824ee6b05630f160128b7bd2b8c0
YaoLi3 <liyaoo6o@gmail.com>
2023-01-17 10:01:57 +0800

    handle nohup.out file, remove redundant lines

--- pgm.sh
+++ pgm.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 date >__start__
-/home/softwares/PGM-1.0.0/PGM /dellfsqd2/ST_OCEAN/USER/liyao1/tools/anaconda3/envs/test/bin/python grn.py 
+/home/softwares/PGM-1.0.0/PGM /dellfsqd2/ST_OCEAN/USER/liyao1/tools/anaconda3/envs/test/bin/python /dellfsqd2/ST_OCEAN/USER/liyao1/stereopy/grn.py 
 #/home/softwares/PGM-1.0.0/PGM /bin/bash test.sh
 date > __end__
 
 /home/softwares/PGM-1.0.0/report.sh > report.txt
 
 rm pglog*
+
+sed -i '/^\ touch/d' nohup.out
+sed -i '/^\ start/d' nohup.out
 
