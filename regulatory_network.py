1510fafef1f0bf60c235476a5d6ba39610560f03
YaoLi3 <liyaoo6o@gmail.com>
2023-02-21 14:17:36 +0800

    add back attrs

--- regulatory_network.py
+++ regulatory_network.py
@@ -44,7 +44,7 @@ from plot_base import PlotBase
 from stereo.core.stereo_exp_data import StereoExpData
 
 
-class Network:
+class GeneNetwork:
     def __init__(self):
         # input
         self.data = None
@@ -92,12 +92,61 @@ class Network:
         return self._auc_mtx
 
 
-class RegulatoryNetwork(AlgorithmBase):
+class InferenceRegulatoryNetwork(AlgorithmBase):
     """
     A gene regulatory network
     """
 
     logger = LogManager(log_path='project.log', level='debug').get_logger(name='Stereo')
+
+    def __init__(self):
+        # input
+        self._data = None
+        self._matrix = None  # pd.DataFrame
+        # network calculated attributes
+        self._regulons = None  # list, check
+        self._auc_mtx = None  # check
+        self._adjacencies = None  # pd.DataFrame
+
+    @property
+    def network(self):
+        return self._network
+
+    @network.setter
+    def network(self, value):
+        self._network = value
+
+    @property
+    def matrix(self):
+        return self._matrix
+
+    @matrix.setter
+    def matrix(self, value):
+        self._matrix = value
+
+    @property
+    def adjacencies(self):
+        return self._adjacencies
+
+    @adjacencies.setter
+    def genes(self, value):
+        self._adjacencies = value
+
+    @property
+    def regulons(self):
+        return self._regulons
+
+    @regulons.setter
+    def regulons(self, value):
+        self._regulons = value
+
+    @property
+    def auc_mtx(self):
+        return self._auc_mtx
+
+    @auc_mtx.setter
+    def auc_mtx(self, value):
+        self._auc_mtx = value
 
     @staticmethod
     def is_valid_exp_matrix(mtx: pd.DataFrame):
@@ -111,32 +160,31 @@ class RegulatoryNetwork(AlgorithmBase):
                 and (mtx.index.nlevels == 1)
                 and (mtx.columns.nlevels == 1))
 
-    @classmethod
-    def load_data(cls, data: Union[StereoExpData, anndata.AnnData]):
+    def load_data(self, data: Union[StereoExpData, anndata.AnnData]):
         """
 
         :param data:
         :return:
         """
-        cls.data = data
+        self.network.data = data
         if isinstance(data, StereoExpData):
-            cls.genes = data.gene_names
-            cls.mtx = data.exp_matrix
-            cls.cells = data.cell_names
+            self.network.genes = data.gene_names
+            self.network.mtx = data.exp_matrix
+            self.network.cells = data.cell_names
         elif isinstance(data, anndata.AnnData):
-            cls.mtx = data.X
-            cls.genes = list(data.var)
+            self.network.mtx = data.X
+            self.network.genes = list(data.var)
 
     @classmethod
-    def load_data_by_cluster(cls, data: Union[StereoExpData, anndata.AnnData], cluster_name: str):
+    def load_data_by_cluster(cls, data: Union[StereoExpData, anndata.AnnData], cluster_label: str, target_clusters:list):
         """
 
         :param data:
-        :param cluster_name:
+        :param cluster_label:
         :return:
         """
         if isinstance(data, anndata.AnnData):
-            pass
+            return data[data.obs[cluster_label].isin(target_clusters)]
 
     @classmethod
     def read_file(cls, fn: str, bin_type='cell_bins'):
@@ -190,6 +238,9 @@ class RegulatoryNetwork(AlgorithmBase):
 
     @classmethod
     def grn_inference(cls,
+                      matrix,
+                      tf_names: list,
+                      genes,
                       num_workers: int,
                       verbose: bool = True,
                       fn: str = 'adj.csv') -> pd.DataFrame:
@@ -204,16 +255,22 @@ class RegulatoryNetwork(AlgorithmBase):
         :param fn:
         :return:
         """
+        #global matrix
         if num_workers is None:
             num_workers = cpu_count()
-        custom_client = RegulatoryNetwork._set_client(num_workers)
-        cls.adjacencies = grnboost2(cls.mtx,
-                                     tf_names=cls.tf_names,
-                                     gene_names=cls.genes,
-                                     verbose=verbose,
-                                     client_or_address=custom_client)
-        cls.adjacencies.to_csv(fn, index=False)  # adj.csv, don't have to save into a file
-        return cls.adjacencies
+        custom_client = InferenceRegulatoryNetwork._set_client(num_workers)
+        # if isinstance(data, StereoExpData):
+        #     matrix = data.exp_matrix
+        # elif isinstance(data, anndata.AnnData):
+        #     matrix = data.X
+        # assert matrix is not None
+        adjacencies = grnboost2(matrix,
+                                tf_names=tf_names,
+                                gene_names=genes,
+                                verbose=verbose,
+                                client_or_address=custom_client)
+        adjacencies.to_csv(fn, index=False)  # adj.csv, don't have to save into a file
+        return adjacencies
 
     @staticmethod
     def _name(fname: str) -> str:
@@ -233,7 +290,7 @@ class RegulatoryNetwork(AlgorithmBase):
         """
         #cls.logger.info('Loading ranked databases...')
         db_fnames = glob.glob(database_dir)
-        dbs = [RankingDatabase(fname=fname, name=RegulatoryNetwork._name(fname)) for fname in db_fnames]
+        dbs = [RankingDatabase(fname=fname, name=InferenceRegulatoryNetwork._name(fname)) for fname in db_fnames]
         return dbs
 
     @classmethod
@@ -249,19 +306,22 @@ class RegulatoryNetwork(AlgorithmBase):
 
     @classmethod
     def ctx_get_regulons(cls,
+                         adjacencies,
+                         matrix,
                          rho_mask_dropouts: bool = False):
         """
         Inference of co-expression modules
         :param rho_mask_dropouts:
         :return:
         """
-        cls.modules = list(
-            modules_from_adjacencies(cls.adjacencies, cls.mtx, rho_mask_dropouts=rho_mask_dropouts)
+        modules = list(
+            modules_from_adjacencies(adjacencies, matrix, rho_mask_dropouts=rho_mask_dropouts)
         )
-        return cls.modules
+        return modules
 
     @classmethod
     def prune(cls,
+              modules,
               dbs: list,
               motif_anno_fn,
               num_workers: int,
@@ -280,11 +340,11 @@ class RegulatoryNetwork(AlgorithmBase):
             num_workers = cpu_count()
         if is_prune:
             with ProgressBar():
-                df = prune2df(dbs, cls.modules, motif_anno_fn, num_workers=num_workers)
+                df = prune2df(dbs, modules, motif_anno_fn, num_workers=num_workers)
             regulons = df2regulons(df)
             df.to_csv(rgn)  # motifs filename
             # alternative way of getting regulons, without creating df first
-            regulons = cls.prune(dbs, cls.modules, motif_anno_fn)
+            regulons = cls.prune(dbs, modules, motif_anno_fn)
             return regulons
         else:
             warnings.warn('if prune is set to False')
@@ -310,12 +370,16 @@ class RegulatoryNetwork(AlgorithmBase):
 
     @classmethod
     def auc_activity_level(cls,
+                           matrix,
+                           regulons,
                            auc_threshold: float,
                            num_workers: int,
                            save: bool = True,
                            fn='auc.csv') -> pd.DataFrame:
         """
 
+        :param matrix:
+        :param regulons:
         :param auc_threshold:
         :param num_workers:
         :param save:
@@ -324,55 +388,64 @@ class RegulatoryNetwork(AlgorithmBase):
         """
         if num_workers is None:
             num_workers = cpu_count()
-        auc_mtx = aucell(cls.mtx, cls.regulons, auc_threshold=auc_threshold, num_workers=num_workers)
+        auc_mtx = aucell(matrix, regulons, auc_threshold=auc_threshold, num_workers=num_workers)
         if save:
             auc_mtx.to_csv(fn)
         return auc_mtx
 
     @classmethod
-    def save_to_loom(cls, loom_fn: str = 'output.loom'):
+    def save_to_loom(cls, matrix, auc_matrix, regulons, loom_fn: str = 'output.loom'):
         """
 
+        :param matrix:
+        :param auc_matrix:
+        :param regulons:
         :param loom_fn:
         :return:
         """
-        export2loom(ex_mtx=cls.mtx, auc_mtx=cls.auc_mtx,
-                    regulons=[r.rename(r.name.replace('(+)', ' (' + str(len(r)) + 'g)')) for r in cls.regulons],
+        export2loom(ex_mtx=matrix, auc_mtx=auc_matrix,
+                    regulons=[r.rename(r.name.replace('(+)', ' (' + str(len(r)) + 'g)')) for r in regulons],
                     out_fname=loom_fn)
 
     @classmethod
-    def uniq_genes(cls):
+    def uniq_genes(cls, adjacencies, matrix:pd.DataFrame):
         """
 
+        :param adjacencies:
+        :param matrix:
         :return:
         """
-        unique_adj_genes = set(cls.adjacencies["TF"]).union(set(cls.adjacencies["target"])) - set(cls.mtx.columns)
+        unique_adj_genes = set(adjacencies["TF"]).union(set(adjacencies["target"])) - set(matrix.columns)
         #RegulatoryNetwork.logger.info(f'find {len(unique_adj_genes) / len(set(cls.mtx.columns))} unique genes')
         return unique_adj_genes
 
     @classmethod
-    def main(cls, data: Union[StereoExpData, anndata.AnnData], databases: str, motif_anno_fn: str, tfs_fn: str):
-        """
+    def main(self,
+             data: Union[StereoExpData, anndata.AnnData],
+             databases: str,
+             motif_anno_fn: str,
+             tfs_fn: str,
+             num_workers: int):
+        """"""
+        global matrix
+        if isinstance(data, StereoExpData):
+            matrix = data.exp_matrix
+        elif isinstance(data, anndata.AnnData):
+            matrix = data.X
+        assert matrix is not None
 
-        :param databases:
-        :param motif_anno_fn:
-        :param tfs_fn:
-        :return:
-        """
-        # sc_exp_fname = os.path.join(resources_folder, 'StereopyData/SS200000135TL_D1.cellbin.gef')
-        # 0. Load StereoExpData file
-        # grn.read_file(sc_exp_fname)
         # 1. load TF list
-        cls.load_tfs(tfs_fn)
+        tfs = self.load_tfs(tfs_fn)
         # 2. load the ranking databases
-        dbs = cls.load_database(databases)
+        dbs = self.load_database(databases)
         # 3. GRN inference
-        cls.grn_inference(num_workers=24)
+        adjacencies = self.grn_inference(matrix, genes=data.var_names, tf_names=tfs, num_workers=num_workers)
         # 4. Regulons prediction aka cisTarget
-        cls.ctx_get_regulons()
-        cls.prune(dbs, motif_anno_fn, num_workers=24)
+        modules = self.ctx_get_regulons(adjacencies, matrix)
+        regulons = self.prune(modules, dbs, motif_anno_fn, num_workers=24)
         # 5: Cellular enrichment (aka AUCell)
-        cls.auc_activity_level(auc_threshold=0.5, num_workers=24)
+        auc_mtx = self.auc_activity_level(matrix, regulons, auc_threshold=0.5, num_workers=num_workers)
+        return adjacencies, regulons, auc_mtx
 
 
 class PlotRegulatoryNetwork(PlotBase):
@@ -426,18 +499,27 @@ class PlotRegulatoryNetwork(PlotBase):
         pass
 
     @staticmethod
-    def dotplot(data: Union[anndata.AnnData, StereoExpData]):
+    def dotplot_anndata(data: Union[anndata.AnnData, StereoExpData],
+                        gene_names,
+                        cluster_label: str,
+                        save: bool = True):
         """
-        create a dotplot for the StereoExpData.
+        create a dotplot for Anndata object.
         a dotplot contains percent (of cells that) expressed (the genes) and average exression (of genes).
-        :param data: a StereoExpData object or an Anndata object.
-        :param: output: the output path. StereoExpData's output will be reset if the output is not None.
-        :return:
+
+        :param data: gene data
+        :param gene_names: interested gene names
+        :param cluster_label: label of clustering output
+        :param save: if save plot into a file
+        :return: plt axe object
+
+        e.g.
+            there is an anndata
         """
         if isinstance(data, anndata.AnnData):
-            return sc.pl.dotplot(data)
+            return sc.pl.dotplot(data, var_names = gene_names, groupby=cluster_label, save=save)
         elif isinstance(data, StereoExpData):
-            return PlotRegulatoryNetwork.dotplot_stereo(data)
+            print('for StereoExpData object, please use function: dotplot_stereo')
 
     @staticmethod
     def auc_heatmap(auc_mtx, fn='auc_heatmap.png'):
