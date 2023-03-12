dcda6f661bfd4df1feee3391cf72768b97437a4a
YaoLi3 <liyaoo6o@gmail.com>
2023-03-12 14:20:03 +0800

    test file

--- stereo/algorithm/test.py
+++ stereo/algorithm/test.py
@@ -1,19 +1,41 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+@file: test.py
+@time: 2023/Mar/09
+@description: test file for inference gene regulatory networks module
+@author: Yao LI
+@email: liyao1@genomics.cn
+@last modified by: Yao LI
+"""
 
+import sys
 import warnings
 warnings.filterwarnings('ignore')
-import stereo
-from stereo.io import read_gem, read_gef
-import sys
-from regulatory_network import InferenceRegulatoryNetwork, PlotRegulatoryNetwork
 
-from stereo.core.stereo_exp_data import StereoExpData
-from stereo.preprocess.qc import cal_qc
+from multiprocessing import cpu_count
+from regulatory_network import InferenceRegulatoryNetwork, PlotRegulatoryNetwork
 
 
 if __name__ == '__main__':
-    fn = sys.argv[1]
-    grn = InferenceRegulatoryNetwork()
-    data = InferenceRegulatoryNetwork.read_file(fn)
-    print(type(data))
+    #
+    tfs_fn = '/dellfsqd2/ST_OCEAN/USER/liyao1/stereopy/resource/tfs/test_mm_mgi_tfs.txt'
+    database_fn = '/dellfsqd2/ST_OCEAN/USER/liyao1/stereopy/database/mm10_10kbp_up_10kbp_down_full_tx_v10_clust.genes_vs_motifs.rankings.feather'
+    motif_anno_fn = '/dellfsqd2/ST_OCEAN/USER/liyao1/stereopy/resource/motifs/motifs-v10nr_clust-nr.mgi-m0.001-o0.0.tbl'
 
+    # h5ad data
+    #fn = '/dellfsqd2/ST_OCEAN/USER/liyao1/stereopy/resource/StereopyData/MouseBrainCellbin.h5ad'
+    #data = InferenceRegulatoryNetwork.load_anndata_by_cluster(fn, 'psuedo_class', ['HBGLU9', 'TEINH12', 'HBSER4'])
+
+    # alternative: small h5ad for testing
+    fn = '/dellfsqd2/ST_OCEAN/USER/liyao1/stereopy/resource/StereopyData/test_grn_MouseBrainCellbin.h5ad'
+    data = InferenceRegulatoryNetwork.read_file(fn)
+
+    # alternative: stereopy data
+
+    # create grn
+    grn = InferenceRegulatoryNetwork(data)
+    grn.main(database_fn, motif_anno_fn, tfs_fn, num_workers=cpu_count())
+
+    grn_plot = PlotRegulatoryNetwork(data)
+    print(grn_plot)
