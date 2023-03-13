860d44756cfa71593326708641eb15f38b22268d
YaoLi3 <liyaoo6o@gmail.com>
2023-03-13 14:16:53 +0800

    test new plotting methods

--- stereo/algorithm/test.py
+++ stereo/algorithm/test.py
@@ -10,9 +10,10 @@
 """
 
 import sys
+import json
 import warnings
 warnings.filterwarnings('ignore')
-
+import pandas as pd
 from multiprocessing import cpu_count
 from regulatory_network import InferenceRegulatoryNetwork, PlotRegulatoryNetwork
 
@@ -23,6 +24,9 @@ if __name__ == '__main__':
     database_fn = '/dellfsqd2/ST_OCEAN/USER/liyao1/stereopy/database/mm10_10kbp_up_10kbp_down_full_tx_v10_clust.genes_vs_motifs.rankings.feather'
     motif_anno_fn = '/dellfsqd2/ST_OCEAN/USER/liyao1/stereopy/resource/motifs/motifs-v10nr_clust-nr.mgi-m0.001-o0.0.tbl'
 
+    # clustering output for stereopy data]
+    meta_fn = '/dellfsqd2/ST_OCEAN/USER/liyao1/stereopy/resource/StereopyData/meta_mousebrain.csv'
+
     # h5ad data
     #fn = '/dellfsqd2/ST_OCEAN/USER/liyao1/stereopy/resource/StereopyData/MouseBrainCellbin.h5ad'
     #data = InferenceRegulatoryNetwork.load_anndata_by_cluster(fn, 'psuedo_class', ['HBGLU9', 'TEINH12', 'HBSER4'])
@@ -31,11 +35,17 @@ if __name__ == '__main__':
     fn = '/dellfsqd2/ST_OCEAN/USER/liyao1/stereopy/resource/StereopyData/test_grn_MouseBrainCellbin.h5ad'
     data = InferenceRegulatoryNetwork.read_file(fn)
 
-    # alternative: stereopy data
-
     # create grn
     grn = InferenceRegulatoryNetwork(data)
-    grn.main(database_fn, motif_anno_fn, tfs_fn, num_workers=cpu_count())
+    grn.main(database_fn, motif_anno_fn, tfs_fn, num_workers=cpu_count(), save=False)
 
     grn_plot = PlotRegulatoryNetwork(data)
-    print(grn_plot)
+
+    meta = pd.read_csv(meta_fn)
+    rdict = json.load(open('regulons.json'))
+    #rdict = grn.regulon_dict
+
+    grn_plot.dotplot_stereo(data, meta, rdict, ['Zfp354c'], ['VLMC2','ABC','ACNT1'], 'cell_type')
+    grn_plot.dotplot_anndata(data, grn.gene_names, cluster_label='psuedo_class')
+    grn_plot.auc_heatmap(grn.auc_mtx)
+    grn_plot.plot_2d_reg_h5ad(data, 'spatial', grn.auc_mtx, 'Zfp354c')
